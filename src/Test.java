94d55bee64bd3ebd438dfa1bb88f5defa8f602a0
vinit <vinit@LAPTOP-V0K7N6A5>
2019-11-20 14:30:23 -0500

    Test push

--- src/Test.java
+++ src/Test.java
@@ -6,6 +6,10 @@ public class Test {
 	public Test(String h) {
 		this.hello = h;
 	}
+	
+	public void testpush() {
+		System.out.println("Heineman");
+	}
 	public void tester() {
 		System.out.println(this.hello);
 	}
